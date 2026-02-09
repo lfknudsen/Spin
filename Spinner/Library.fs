49dacd83c5b3a816114b84959f57463151c1c82e
Louis Falk Knudsen <louis@falkknudsen.com>
2026-02-09 20:17:40 +0100

    Slight performance optimisations in `_spin`

--- Spinner/Library.fs
+++ Spinner/Library.fs
@@ -1,30 +1,33 @@
 ﻿module Spinner
 
+open System
 open System.Threading
 open System.Threading.Channels
 
-let private spinnerCharacters = [|'|'; '/'; '-'; '\\'|]
+let private spinnerCharacters = [|"\r|"; "\r/"; "\r-"; "\r\\"|]
+
+let private delayDurationMs = int (TimeSpan.FromSeconds(0.15).TotalMilliseconds)
 
 // Endlessly loop, updating the current line output each time.
 // The current thread sleeps for 0.15 seconds each time.
-let rec private _spin (chars : char array) (i : int) (wait : ChannelReader<unit>) =
-    if wait.Count = 0 then
-        let idx = i % chars.Length;
-        printf $"\r%c{chars[idx]}"
-        Thread.Sleep(System.TimeSpan.FromSeconds(0.15))
-        _spin chars (idx + 1) wait
-    else
-        printf "\r"
+let private _spin (chars : string array) (wait : ChannelReader<unit>) =
+    let mutable idx = 0
+    while wait.Count = 0 do
+        idx <- idx % chars.Length;
+        Console.Write(chars[idx])
+        Thread.Sleep(delayDurationMs)
+        idx <- idx + 1
+    printf "\r"
 
 // Starts a new spinner, which will continue until anything is sent to the
 // given channel.
 let public spin (wait : ChannelReader<unit>) (_ : obj) : unit =
-    _spin spinnerCharacters 0 wait
+    _spin spinnerCharacters wait
 
 // Starts a new spinner, which will continue until anything is sent to the
 // given channel.
 let public spinBackwards (wait : ChannelReader<unit>) (_ : obj) : unit =
-    _spin (Array.rev spinnerCharacters) 0 wait
+    _spin (Array.rev spinnerCharacters) wait
 
 // Creates - but does not start - a background thread which will supply the
 // given function with a channel reader.
